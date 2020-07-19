f20dd4c5e50f13cb035ebb9da6eab13f6b8654f9
ihetal <hetal.shah@sjsu.edu>
2020-07-18 17:26:55 -0700

    Initial commit

---
+++ sample_data.py
@@ -0,0 +1,47 @@
+USERS = [
+    {
+        'name': 'John Smith',
+        'email': 'jsmith@gmail.com',
+        'age': 20
+    },
+    {
+        'name': 'Bari Pushard',
+        'email': 'bpushard@gmail.com',
+        'age': 21
+    },
+    {
+        'name': 'Irish Rackers',
+        'email': 'irackers@gmail.com',
+        'age': 22
+    },
+    {
+        'name': 'Agueda Letsinger',
+        'email': 'aletsinger@gmail.com',
+        'age': 23
+    },
+    {
+        'name': 'Lisbeth Stacker',
+        'email': 'lstacker@gmail.com',
+        'age': 24
+    },
+    {
+        'name': 'John Smith',
+        'email': 'jsmith@gmail.com',
+        'age': 20
+    },
+    {
+        'name': 'Hetal Smith',
+        'email': 'jhetasmith@gmail.com',
+        'age': 16
+    },
+    {
+        'name': 'Rohan Smith',
+        'email': 'rohanjsmith@gmail.com',
+        'age': 40
+    },
+    {
+        'name': 'David Smith',
+        'email': 'davjsmith@gmail.com',
+        'age': 'abcd'
+    }
+]
